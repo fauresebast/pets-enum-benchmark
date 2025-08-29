a10e7d3468162accd6b6d3bf0066068b5d606535
Sebastien Faure <sebastien.faure@pennylane.com>
2025-08-29 14:52:12 +0200

    Generate data in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "[ db/seeds.rb ] Deleting database"
+PetInteger.delete_all
+PetIntegerIndex.delete_all
+PetPgsqlEnum.delete_all
+PetPgsqlEnumIndex.delete_all
+PetString.delete_all
+PetStringIndex.delete_all
+
+NUM_OF_RECORDS = 100_000
+
+puts "[ db/seeds.rb ] Seeding database: preparing data"
+columns = [:name, :weight, :birthday, :kind]
+generated_data = (0..NUM_OF_RECORDS).map do |i|
+  {
+    name: Faker::Creature::Dog.name,
+    weight: rand(1.0..100.0),
+    birthday: Faker::Date.birthday(min_age: 1, max_age: 20),
+    kind_id: Random.rand(4)
+  }
+end
+pet_integer_kinds = generated_data.map { |data| [data[:name], data[:weight], data[:birthday], data[:kind_id]] }
+
+puts "[ db/seeds.rb ] Seeding database: creating #{NUM_OF_RECORDS} records per model"
+
+PetInteger.import columns, pet_integer_kinds
+PetIntegerIndex.import columns, pet_integer_kinds
+puts "[ db/seeds.rb ] PetInteger created"
+
+kind = %w[cat dog rabbit owl].freeze
+pet_string_kinds = generated_data.map { |data| [data[:name], data[:weight], data[:birthday], kind[data[:kind_id]]] }
+
+PetPgsqlEnum.import columns, pet_string_kinds
+PetPgsqlEnumIndex.import columns, pet_string_kinds
+puts "[ db/seeds.rb ] PetPgsqlEnum created"
+
+PetString.import columns, pet_string_kinds
+PetStringIndex.import columns, pet_string_kinds
+puts "[ db/seeds.rb ] PetString created"
+
+PetStatistic.refresh
+puts "[ db/seeds.rb ] PetStatistic refreshed"
+
+puts "[ db/seeds.rb ] Database seeded"
